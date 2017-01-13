683ab47fc76c3ffbcfef2740f1a6aa1fe0d0b967
Kate Denier <katyi.pup@gmail.com>
2017-01-13 15:28:50 +0000

    updated

--- SlackBot v2.js
+++ SlackBot v2.js
@@ -7,7 +7,7 @@ var settings = {
 };
 
 var bot = new Bot(settings);
-var intel = [];
+var intel = ["place","holder","test","stuff","delete","later"];
 var verified = ["U2CLWCW7L", "U2CLC69U6", "U2CL9P7QU"];
 var trying = [];
 var reply = ""
@@ -31,53 +31,60 @@ function isVerified(userID){
 }
 //currently not used; will be the names to get stuff from server lol it doesn't actually do anything or work for anything rn.
 function findStats(myInfo, myStats){
-	for(var i=0; i<myQueue.length; i++){
-		if(myQueue[i].indexOf(myUser) === 0){
+	for(var i=0; i<myStats.length; i++){
+		if(myStats[i].indexOf(myInfo) === 0){
 			return i;
 		}
 	}
 	return -1;
 }
-//need one to find what chat a message is posted in
-function replyWhere(data){
-	if(data.channels !== -1){
-		let channel = data.channels
-		bot.postMessageToChannel(channel.toString, reply)
-	}
-	else if(data.groups !== -1){
-		let group = data.groups
-		return typeof data.groups
-		bot.postMessageToGroup(group.toString, reply)
-	}
 
+//working now
+function findChannel(channelId){
+	switch(channelId){
+		case "G3MENRSHH":
+		bot.postMessageToGroup('scouting_app_testing', reply);
+		break;
+		case "C3MJX9J2G":
+		bot.postMessageToChannel('scouting_testing', reply);
+		break;
+		default:
+		console.log("nope")
+		break;
+	}
 }
 
-//testing stuff IT DOESNT WORK WPUEHWFIDSJ
+
 bot.on("message", function(data){
 	var input = data.text;
+	var r = data.channel;
+
+	//basic testing phrase
 if(input === "HELLO PLZ"){
-	trying.push(data.user)
 	reply = "working!"
-	replyWhere(data)
+	findChannel(r);
 	
 }
 //main function
-if(input.indexOf("robostats #") !== -1){
-	let args = input.split(" ").slice(2);
+if(input.indexOf("robostats") !== -1){
+	let args = input.split(" ").slice(1);
 			//tried to parse the command into an integer
 			let statsId = Number.parseInt(args[0]);
 			//verifies if its an integer
 			if(Number.isInteger(statsId)) {
 				//NEED TO CHANGE\/ (checks to see if the int is within the queue length) to CHECKS TO SEE IF VALID NUMBER
-				if (statsId <= intel.length) {
-					//removes thing. needs to change to gives the thing
-					intel.splice((intelId - 1), 1);
-					bot.postMessageToGroup("scouting_app_testing", "Stats for Team " + statsId + " have been targeted! katakatakata...this is it! (this is why you don't put Kate in charge of dialogue)");
+				if (statsId <= intel.length) { //it'll be findStats(statsId)
+					//removes thing\/ needs to change to gives the thing
+					reply = "Stats for Team " + statsId + " have been targeted! katakatakata...this is it! (this is why you don't put Kate in charge of dialogue)\n" + intel[statsId - 1].toString();
+					findChannel(r);
+					//also send the data/for now the array thing
 				} else {
-					bot.postMessageToGroup("scouting_app_testing", "There's...nothing?! We either don't have information on them or they don't exist. Spooky.");
+					reply = "There's...nothing?! We either don't have information on them, or they don't exist. Spooky.";
+					findChannel(r);
 				}
 			} else {
-						bot.postMessageToGroup("scouting_app_testing", "Sorry, you need to say a number to remove it.");
+					reply = "Sorry, you need to say a number to remove it.";
+					findChannel(r);
 					}
 }
 
@@ -92,14 +99,16 @@ else if (input.startsWith("~add info"))
 
 			intel.push(" [" + information + "]\n");
 			intel[index] = "#" + infoId + " [" + information + "]\n"
-			bot.postMessageToGroup("scouting_app_testing", "Thanks!");
+			reply = "Thanks!";
+			findChannel(r);
 }
 
 else if (input.indexOf("~intel") === 0)
 	{
 		if (intel.length === 0)
 		{
-			bot.postMessageToGroup("scouting_app_testing", "It's empty.");
+			reply = "It's empty.";
+			findChannel(r);
 		}
 		else
 		{
@@ -109,7 +118,8 @@ else if (input.indexOf("~intel") === 0)
 				var myIndex = i + 1;
 				intelstring = intelstring + "#" + myIndex + ": " + intel[i] + "\n";
 			}
-			bot.postMessageToGroup("scouting_app_testing", "this is the correct method !!!111! here:\n" + intelstring);
+			reply = "This is the list of things people noticed and recorded with ~add info:\n" + intelstring;
+			findChannel(r);
 		}
 	}
 
@@ -119,18 +129,21 @@ else if (input === "~read")
 		{
 		if(intel.length > 0) {
 			intel.length = 0;
-			bot.postMessageToGroup("scouting_app_testing", "Good job! It's empty now.");
+			reply = "Good job! It's empty now.";
+			findChannel(r);
 			}
 		else
-		bot.postMessageToGroup("scouting_app_testing", "It's already empty!!");
+		reply = "It's already empty!!";
+		findChannel(r);
 		}
 	else{
 		trying.push(data.user)
-		bot.postMessageToGroup("scouting_app_testing", "You aren't verified for this role. Contact @kate_denier if you think this is a mistake.");
+		reply = "You aren't verified for this role. Contact @kate_denier if you think this is a mistake.";
+		findChannel(r);
 		}
 }
 
-else if(input.startsWith("~robostats removal")){
+else if(input.startsWith("~info removal")){
 	let args = input.split(" ").slice(2);
 			//tried to parse the command into an integer
 			let intelId = Number.parseInt(args[0]);
@@ -141,9 +154,11 @@ else if(input.startsWith("~robostats removal")){
 				if (intelId <= intel.length) {
 					//removes thing.
 					intel.splice((intelId - 1), 1);
-					bot.postMessageToGroup("scouting_app_testing", "Target " + intelId + " has been removed! PewPewPew!");
+				reply = "Target " + intelId + " has been removed! PewPewPew!";
+				findChannel(r);
 				} else {
-					bot.postMessageToGroup("scouting_app_testing", "That isn't valid???");
+					reply = "That isn't valid???";
+					findChannel(r);
 				}
 			} else {
 						bot.postMessageToGroup("scouting_app_testing", "Sorry, you need to say a number to remove it.");
@@ -160,5 +175,4 @@ else if(input === "~verifying"){
 	bot.postMessageToGroup("scouting_app_testing", "This is the list of those who tried to use a verified only command without success.\n" + trying.join("\n"));
 }
 });
-
 
